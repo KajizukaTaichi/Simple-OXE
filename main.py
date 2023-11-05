db6172cf1c9f142f3afa6eec72fb2873b913669d
梶塚太智 <122075081+KajizukaTaichi@users.noreply.github.com>
2023-11-05 17:33:45 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ app = Flask("Simpleオンライン環境")
 
 @app.route("/")
 def index():
-    return render_template("index.html", code="print 'Hello, world'",reply="")
+    with open(r"C:\Users\admin\OneDrive\デスクトップ\プログラミング\Python\プログラム\Simpleオンライン環境\code.smp", newline='\n', mode="r", encoding="utf-8") as f:
+        code=f.read()
+    return render_template("index.html", code=code,input=" ",reply=" ")
 
 @app.route("/", methods=["POST"])
 def execute():
     code = request.form.get("code")
+    input_data = request.form.get("input")
     print(code)
 
-    with open(r"C:\Users\admin\OneDrive\デスクトップ\プログラミング\Python\プログラム\Simpleオンライン環境\code.smp", mode="w", encoding="utf-8") as f:
-        f.write(code)
+    with open(r"C:\Users\admin\OneDrive\デスクトップ\プログラミング\Python\プログラム\Simpleオンライン環境\code.smp", newline='\n', mode="w", encoding="utf-8") as f:
+        f.write(code.replace("\n\n", "\n"))
 
     # 実行したいexeファイルのパス
     exe_path: str = r"C:\Users\admin\OneDrive\デスクトップ\プログラミング\Rust\インタプリタ\target\release\simple.exe"
@@ -22,9 +25,9 @@ def execute():
     args = [r"C:\Users\admin\OneDrive\デスクトップ\プログラミング\Python\プログラム\Simpleオンライン環境\code.smp"]
 
     # subprocess.runを使用してexeを実行
-    result = subprocess.run([exe_path] + args, capture_output=True, text=True, encoding='utf-8')
+    result = subprocess.run([exe_path] + args, input=input_data, capture_output=True, text=True, encoding='utf-8')
     
     print(result.stdout)
-    return render_template("index.html", code=code, reply = result.stdout)
+    return render_template("index.html", code=code, input=input_data, reply = result.stdout)
 
-app.run(host="127.0.0.1", port=8888, debug=True)
+app.run(host="0.0.0.0", port=8888, debug=True)
